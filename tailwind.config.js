8da32bb9b4581d96e906bf157aeefd129276bfe9
Pablo Fonseca <fonsecasilvapab@gmail.com>
2024-05-05 16:04:26 +0200

    Basic layout setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'nohemi': ['Nohemi', 'sans-serif'],
+                'mango-grotesque-black': ['Mango-Grotesque-Black', 'sans-serif'],
+                'mango-grotesque-bold': ['Mango-Grotesque-Bold', 'sans-serif']
+            }
+        },
     },
     plugins: [],
 }
